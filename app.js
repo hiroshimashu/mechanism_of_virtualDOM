81a41497795723b375314fbc0cf3b87b0023c146
hiroshimashu <hiroshimashu@gmail.com>
2018-12-24 20:29:00 +0900

    implement updateElement

--- app.js
+++ app.js
@@ -8,3 +8,27 @@ function createElement(node) {
         .forEach($el.appendChild.bind($el));
     return $el;
 }
+
+function updateElement($parent, newNode, oldNode, index = 0) {
+    if (!oldNode) {
+        $parent.appendChild(
+            createElement(newNode)
+        );
+    } else if (!newNode) {
+        $parent.removeChild(
+            $parent.childNdoes[index]
+        );
+    } else if (changed(newNode, oldNode)) {
+        $parent.replaceChild(
+            createElement(newNode),
+            $parent.childNdoes[index]
+        );
+    }
+}
+
+function changed(node1, node2) {
+    return typeof node1 !== typeof node2 ||
+           typeof node1 === typeof 'string' && node1 !== node2 ||
+           node1.type !== node2.type
+}
+
